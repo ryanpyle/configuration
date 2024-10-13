c874bf32bc056f080f61bd75d1463b1d5a7dbca0
Ryan Pyle <ryanpyle7@gmail.com>
2024-10-13 12:17:35 -0500

    Switched to lazy config

---
+++ nvim/lua/plugins/color.lua
@@ -0,0 +1,12 @@
+return {
+  -- add everforest
+  { "neanias/everforest-nvim" },
+
+  -- Configure LazyVim to load everforest``
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
