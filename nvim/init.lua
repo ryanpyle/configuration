edb6338b149f66d4b9ebc7f3a011a34fa16a9b6a
Ryan Pyle <125594474+ryanpyle@users.noreply.github.com>
2025-08-10 21:31:36 -0500

    updated

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,44 +1,42 @@
--- Colorscheme
-vim.opt.termguicolors = true
-vim.cmd("colorscheme onehalfdark")
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Lazy (Package Manager)
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  -- Which-Key
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    keys = {
-        {
-          "<leader>?",
-          function()
-	    require("which-key").show({ global = false })
-          end,
-          desc = "Buffer Local Keymaps (which-key)",
-        },
-      },
-    },
- 
-  -- Mini Icons (Which-Key Optional Requirement)
-  { 'echasnovski/mini.nvim', version = false },
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-  -- nvim-web-devicons (Which-Key Optional Requirement)
-  "nvim-tree/nvim-web-devicons",
-})
+-- Setup lazy.nvim
+local plugins = {
+	{ "RRethy/base16-nvim", name = "base1e-woodland", priority = 1000 },
+	{ "mason-org/mason-lspconfig.nvim",
+    	   opts = {
+            ensure_installed = { "lua_ls", "rust_analyzer" },
+    	   },
+           dependencies = {
+           { 
+	    "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+           },
+       },
+}
+
+require("lazy").setup(plugins, {})
+
+vim.cmd("colorscheme base16-gruvbox-dark-hard")
 
